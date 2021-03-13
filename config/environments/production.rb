4e93e7c0a505fe713354d42dd27b5f7eba63d7e3
Lawrence <lawrencemurry@yahoo.com>
2021-03-13 22:29:12 +0000

    Trying to get signup/login function to work in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,14 @@
 Rails.application.configure do
+    config.paperclip_defaults = {
+    :storage => :s3,
+    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_region => ENV['AWS_REGION']
+    }
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -13,11 +23,6 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  
-  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
-  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
-  # `config/secrets.yml.key`.
-  config.read_encrypted_secrets = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -25,10 +30,11 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -43,6 +49,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # config.web_socket_server_url = "wss://action-cable-airbnbwho.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://action-cable-airbnbwho.herokuapp.com/cable', 'https://action-cable-airbnbwho.herokuapp.com/cable' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -59,7 +67,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "pinteresting_2_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "AirPikachu_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -76,16 +84,42 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
+  
+  #Required for Heroku
+  # config.action_mailer.default_url_options = { host: 'https://airbnbwho.herokuapp.com' }
 
+  config.action_mailer.raise_delivery_errors = true
+  
+    # config.action_mailer.delivery_method = :smtp
+    # host = 'airbnbwho.herokuapp.com'
+    # config.action_mailer.default_url_options = { host: host }
+    # ActionMailer::Base.smtp_settings = {
+    #   :address        => 'smtp.sendgrid.net',
+    #   :port           => '587',
+    #   :authentication => :plain,
+    #   :user_name      => ENV['SENDGRID_USERNAME'],
+    #   :password       => ENV['SENDGRID_PASSWORD'],
+    #   :domain         => 'https://airbnbwho.herokuapp.com',
+    #   :enable_starttls_auto => true
+    # }
+  
+   ApplicationMailer.notify_user("info@airbnbwho.com").deliver_later
+ 
+    ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'airbnbwho.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp  
+  
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
 end
