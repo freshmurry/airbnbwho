aeb75cd8cafb000ce49c8a3686b02001b9836db4
Lawrence <lawrencemurry@yahoo.com>
2022-01-22 22:57:55 +0000

    Updated gems with bundle update and chacged rails gem from 5.0.3 to 5.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,10 +9,10 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports and disable caching.
+  # Show full error reports.
   config.consider_all_requests_local = true
-  
-   # Enable/disable caching. By default caching is disabled.
+
+  # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
@@ -27,15 +27,12 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  
-  config.action_controller.perform_caching = false
-  
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-  
-  # Send emails using letteropener in dev mode to open emails in a browser
-  config.action_mailer.delivery_method = :letter_opener
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
@@ -45,43 +42,13 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to expire them through the digest params.
-  config.assets.digest = true
-
-  # Adds additional error checking when serving assets at runtime.
-  # Checks for improperly declared sprockets dependencies.
-  # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
-
+  # Suppress logger output for asset requests.
   config.assets.quiet = true
-  
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  
+
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # config.action_cable.url = "wss://airbnbwho.herokuapp.com/cable"
-  # config.action_cable.url = 'wss://example.com/cable'
-  
-  config.web_console.whitelisted_ips << "10.240.0.0/16"
-  
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  config.action_mailer.default_url_options = { host: 'https://54664f58b34a4690950a4ef07f1ca29e.vfs.cloud9.us-east-1.amazonaws.com' }
-
-  # Mailgun Integration
-  
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.mailgun.org',
-    port: 2525,
-    domain: 'sandbox849ca54dc98b42888da6976e40db3c35.mailgun.org',
-    authentication: 'plain',
-    user_name: 'postmaster@sandbox849ca54dc98b42888da6976e40db3c35.mailgun.org',
-    password: '20e436f9190a67c9fbb9b852ab52cae8'
-  }
-  
-  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
-  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 end
