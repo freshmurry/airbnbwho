2fe8236c2abc2dda5ec6c5149239bd5f4212c248
Lawrence Murry <lawrencemurry@yahoo.com>
2024-08-29 20:11:53 -0500

    Trying to fix csrf error

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# Rails.application.config.session_store :cookie_store, key: '_pinteresting_2_session'
-Rails.application.config.session_store :cookie_store, key: '_airbnbwho_session', secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_airbnbwho_session'
 
 # Rails.application.config.session_store :active_record_store, key: '_airbnbwho_session', secure: Rails.env.production?
 
