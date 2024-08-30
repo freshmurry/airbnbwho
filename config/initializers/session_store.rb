846ada028121e3ab5229cdf667d58c5dffe952c2
Lawrence Murry <lawrencemurry@yahoo.com>
2024-08-29 22:02:49 -0500

    Fixing image issue

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,3 @@
 # Rails.application.config.session_store :cookie_store, key: '_AirbnbWho_session'
 
 Rails.application.config.session_store :cookie_store, key: '_AirbnbWho_session', secure: Rails.env.production?
-
-# Rails.application.config.session_store :active_record_store, key: '_AirbnbWho_session', secure: Rails.env.production?
-
-# Rails.application.config.session_store :cookie_store, key: '_airbnbwho_session', domain: :all, tld_length: 2
