f3208ae1afe62053de5e1616bdf593f23e5317d6
Lawrence Murry <lawrencemurry@yahoo.com>
2024-08-29 21:53:02 -0500

    Fixing error

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_airbnbwho_session'
+# Rails.application.config.session_store :cookie_store, key: '_airbnbwho_session'
 
-# Rails.application.config.session_store :active_record_store, key: '_airbnbwho_session', secure: Rails.env.production?
+Rails.application.config.session_store :active_record_store, key: '_AirbnbWho_session', secure: Rails.env.production?
 
-Rails.application.config.session_store :cookie_store, key: '_airbnbwho_session', domain: :all, tld_length: 2
+# Rails.application.config.session_store :cookie_store, key: '_airbnbwho_session', domain: :all, tld_length: 2
