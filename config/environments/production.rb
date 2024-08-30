a1208d4bdfc8e9b4acf32634d479c9eb7c9199ad
Lawrence Murry <lawrencemurry@yahoo.com>
2024-08-30 18:18:22 -0500

    Updated views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,15 +93,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   
     config.action_mailer.delivery_method = :smtp
-    host = 'airbnbwho-bddd833040881.herokuapp.com'
-    config.action_mailer.default_url_options = { host: 'airbnbwho-bddd833040881.herokuapp.com' }
+    host = 'airbnbwho-bdd833040881.herokuapp.com'
+    config.action_mailer.default_url_options = { host: 'airbnbwho-bdd833040881.herokuapp.com' }
     ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
       :user_name      => ENV['SENDGRID_USERNAME'],
       :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'airbnbwho-bddd833040881.herokuapp.com',
+      :domain         => 'airbnbwho-bdd833040881.herokuapp.com',
       :enable_starttls_auto => true
     }
   
