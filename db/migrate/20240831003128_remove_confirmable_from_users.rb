452301bc2cc17319e4f82d5e23aad394da4f7a5a
Lawrence Murry <lawrencemurry@yahoo.com>
2024-08-31 20:09:38 -0500

    Removed a databae table

--- db/migrate/20240831003128_remove_confirmable_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveConfirmableFromUsers < ActiveRecord::Migration[5.0]
-  def change
-    remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -192,6 +192,7 @@ ActiveRecord::Schema.define(version: 20240831225030) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
