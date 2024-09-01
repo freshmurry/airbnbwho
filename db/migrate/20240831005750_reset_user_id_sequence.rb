b197a0983038735fed41d0cf42d7cebea37f443b
Lawrence Murry <lawrencemurry@yahoo.com>
2024-08-31 20:49:55 -0500

    Updated database table

--- db/migrate/20240831005750_reset_user_id_sequence.rb
+++ db/migrate/20240831005750_reset_user_id_sequence.rb
@@ -1,10 +1,17 @@
 class ResetUserIdSequence < ActiveRecord::Migration[5.0]
   def up
-    # Replace `users` with the name of your table if different
-    execute("DELETE FROM sqlite_sequence WHERE name = 'users'")
+    case ActiveRecord::Base.connection.adapter_name
+    when 'PostgreSQL'
+      execute("SELECT setval(pg_get_serial_sequence('users', 'id'), coalesce(max(id), 1), false) FROM users;")
+    when 'SQLite'
+      # SQLite-specific logic, if needed
+      # For SQLite, resetting sequences might not be necessary in Rails migrations
+    else
+      raise "Unsupported database adapter: #{ActiveRecord::Base.connection.adapter_name}"
+    end
   end
 
   def down
-    # No need to define anything here
+    # Define how to reverse the migration if needed
   end
 end
